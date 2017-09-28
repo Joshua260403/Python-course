d68e56ed9a9ac291919193cae15b0c8bf8d15265
Joshua260403 <joshuavwaardenberg@gmail.com>
2017-09-28 20:51:08 +0200

    Add files via upload

---
+++ Tutor1(print en input).py
@@ -0,0 +1,86 @@
+import time
+import random
+
+
+# "print" gebruik je om de shell iets te laten zeggen.
+# print()
+# In print gebruik je \n om naar de volgende regel te gaan.
+# Je kan in plaats van \n ook een tweede print commando gebruiken.
+# LET OP: Om de shell een tekst op te laten lezen gebruik je quotes "..."!
+
+# Opdracht 1
+# Laat de shell "Hello world!" en "Hallo wereld!" zeggen op een andere regel.
+
+
+
+
+
+
+# Je kan de shell ook variabelen laten oplezen.
+# Hiervoor gebruik je print() maar zonder de quotes "...".
+# Een variabele maken doe je zo:
+# y = 1
+# Hierbij wordt de waarde van y 1.
+# Een variabele kan ook tekst bevatten:
+# y = "Bonjour tout le monde"
+# LET OP: Ook hier heb je quotes "..." nodig!
+
+# Opdracht 2
+# Maak een variabele genaamd z, geef deze een waarde onder de 10 en print deze.
+
+
+
+
+
+
+# Om de gebruiker iets in te laten vullen, gebruik je input.
+# Hiermee kan je dus de gebruiker een variabele laten maken:
+# naam = input()
+# Tussen de boogjes kan je een vraag stellen of bericht typen
+# waar de gebruiker dan antwoord op geeft, dat werkt dus hetzelfde als print.
+
+# Als je tekst en een variabele achter elkaar op een regel wilt printen,
+# dan doe je dat zo:
+# print("Hallo " + naam + "!")
+# LET OP: Zet een spatie achter Hallo anders print de shell het volgende:
+# "Hallo[naam]!" (zonder spaties)
+
+# Opdracht 3
+# Laat de shell vragen naar de naam van de gebruiker en laat de shell dan
+# "Hallo [naam]! zeggen.
+
+
+
+
+
+
+# Je kan ook rekenen met variabelen:
+# a = 2
+# b = 2
+# c = a + b
+# c is hier dus 4.
+# LET OP:
+# Vermenigvuldigen is *
+# Delen door is /
+# Optellen is +
+# Aftrekken is -
+
+# Opdracht 4
+# Laat de shell vragen naar x en naar y en print dan de oppervlakte (x keer y)
+# LET OP: Heb je al eerder een variabele x of y genoemd? gebruik dan iets anders!
+
+
+
+
+
+
+# EINDOPDRACHT
+# Laat de shell vragen naar de leeftijd van de gebruiker en het huidige jaar.
+# Reken hiermee het geboortejaar uit en print deze.
+
+
+
+
+
+
+
